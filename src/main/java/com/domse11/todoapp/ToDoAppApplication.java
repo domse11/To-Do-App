94753804033f42fd6e3f5bea4f8636406a301fc2
Dominique Mariacher <dmariacher11@gmail.com>
2025-04-03 16:12:53 +0200

    Spring Boot Grundstruktur mit Postgres angebunden

---
+++ src/main/java/com/domse11/todoapp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.domse11.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
